06fd7b64a97df04019943200379550b8ad776ed7
Julia Assunção <juliaassuncao256@gmail.com>
2025-05-24 16:06:16 -0300

    feat(header): adiciona animação e finaliza Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,16 +1,58 @@
-import styles from "./Header.module.scss"
+import { useEffect, useRef } from "react";
+import { gsap } from "gsap";
+
+import styles from "./Header.module.scss";
 
 export default function Header() {
+  const primaryRef = useRef<HTMLSpanElement>(null);
+  const secondaryRef = useRef<HTMLSpanElement>(null);
+  const backgroundRef = useRef<HTMLSpanElement>(null);
+
+  useEffect(() => {
+    const tl = gsap.timeline();
+
+    tl.from(primaryRef.current, {
+      opacity: 0,
+      y: -20,
+      duration: 0.4,
+      ease: "power3.out",
+    })
+      .from(
+        secondaryRef.current,
+        {
+          opacity: 0,
+          y: -20,
+          duration: 0.4,
+          ease: "power3.out",
+        },
+        "-=0.2"
+      )
+      .from(
+        backgroundRef.current,
+        {
+          opacity: 0,
+          duration: 0.7,
+          ease: "sine.out",
+        },
+        "+=0.1"
+      );
+  }, []);
+
   return (
     <header className={styles.header}>
       <div className={styles.container}>
         <div className={styles.mainTitle}>
-          <span className={styles.primaryText}>A Melhor Faculdade</span>
-          <span className={styles.secondaryText}>de Tecnologia</span>
+          <span ref={primaryRef} className={styles.primaryText}>
+            A Melhor Faculdade
+          </span>
+          <span ref={secondaryRef} className={styles.secondaryText}>
+            de Tecnologia
+          </span>
         </div>
-        
-        <span className={styles.backgroundText}>Sobre</span>
+        <span ref={backgroundRef} className={styles.backgroundText}>
+          Sobre
+        </span>
       </div>
     </header>
-  )
+  );
 }
