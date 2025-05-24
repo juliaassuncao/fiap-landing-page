8814c6a44c3a0625feea041fef191a31373a5b16
Julia Assunção <juliaassuncao256@gmail.com>
2025-05-24 10:58:41 -0300

    feat: cria componente ProgressBar e adiciona lógica de estado

---
+++ src/components/Navbar/ProgressBar.tsx
@@ -0,0 +1,24 @@
+"use client"
+
+import { useEffect, useState } from "react"
+import styles from "./ProgressBar.module.scss"
+
+export default function ProgressBar() {
+  const [scrollProgress, setScrollProgress] = useState(0)
+
+  useEffect(() => {
+    function handleScroll() {
+      const scrollTop = window.scrollY
+      const docHeight = document.documentElement.scrollHeight - window.innerHeight
+      const scrollPercent = (scrollTop - docHeight) * 100
+      setScrollProgress(scrollPercent)
+    }
+
+    window.addEventListener("scroll", handleScroll)
+    return () => window.removeEventListener("scroll", handleScroll)
+  }, [])
+
+  return (
+    <div className={styles.progress} style={{width: `${scrollProgress}%`}}></div>
+  )
+}
