aa8185d87ee71ab7311403949a2f185ef21fd802
Julia Assunção <juliaassuncao256@gmail.com>
2025-05-24 23:54:07 -0300

    refactor: reorganiza estrutura de pastas e arruma importações

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,8 @@ import "./styles/globals.scss";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Landing Page - FIAP",
-  description: "Projeto de avaliação técnica para vaga de FrontEnd",
+  title: "FIAP",
+  description: "Landing page desenvolvida como parte de uma avaliação técnica para vaga de Front-End, com foco em performance, design responsivo e interatividade.",
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import styles from "./page.module.scss"
+
 import Courses from "@/app/components/Courses/Courses";
 import Header from "@/app/components/Header/Header";
 import Intro from "@/app/components/Intro/Intro";
