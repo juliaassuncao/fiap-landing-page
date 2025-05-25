80c7d08dadf727cbe2416eb8a7d1639b7ad34d25
Julia Assunção <juliaassuncao256@gmail.com>
2025-05-25 00:06:40 -0300

    feat: define interfaces Course e Tab e adiciona estrutura de tabs com cursos categorizados

--- src/app/components/Courses/Courses.tsx
+++ src/app/components/Courses/Courses.tsx
@@ -1,17 +1,47 @@
+"use client"
+
+import { Tab } from "./Courses.interface";
 import styles from "./Courses.module.scss"
+
+const TABS: Tab[] = [
+  {
+    title: "Tecnologia",
+    courses: [
+      { title: "Big Data Ecosystem", type: "REMOTO • LIVE" },
+      { title: "Creating Dashboards for BI", type: "REMOTO • LIVE" },
+      {
+        title: "Big Data Science - Machine Learning & Data Mining",
+        type: "REMOTO • LIVE • MULTIMÍDIA",
+      },
+      { title: "Storytelling", type: "REMOTO • LIVE" },
+    ],
+  },
+  {
+    title: "Inovação",
+    courses: [
+      { title: "UX", type: "Digital" },
+      { title: "UX Writing", type: "LIVE" },
+      { title: "Storytelling para Negócios", type: "LIVE" },
+      { title: "Chatbots", type: "LIVE" },
+    ],
+  },
+  {
+    title: "Negócios",
+    courses: [
+      { title: "Agile Culture", type: "Digital" },
+      { title: "DPO Data Protection Officer", type: "LIVE" },
+      { title: "IT Business Partner", type: "LIVE" },
+      { title: "Perícia Forense Computacional", type: "LIVE" },
+    ],
+  },
+];
 
 export default function Courses() {
   return (
-    <div className={styles.container}>
-      <div className={styles.titleWrapper}>
-        <h1 className={styles.title}>Cursos</h1>
-        <h2 className={styles.subtitle}>Cursos de Curta Duração</h2>
+    <section className={styles.container}>
+      <div>
+
       </div>
-      <ul className={styles.menu}>
-        <li className={styles.menuItem}>Tecnologia</li>
-        <li className={styles.menuItem}>Inovação</li>
-        <li className={styles.menuItem}>Negócios</li>
-      </ul>
-    </div>
+    </section>
   )
 }
